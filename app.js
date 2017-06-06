7fe121a2c7888f26a0b36c3b668bc061486799cd
kromero16 <kevkvn13@live.com>
2017-06-06 10:30:06 -0700

    Added navbar

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
 var main=function(){
-	"use strict";
-
-var addCommentFromInputBox = function(){
-	var $new_comment;
-
-	if($(".comment-input input") !== ""){
-		$new_comment = $("<p>").text($(".comment-input input").val());
-		$new_comment.hide();
-		$(".comments").append(new_comment);
-		$new_comment.fadeIn();
-		$(".comment-input input").val("");
-	}
-}
+	
 
 }
 $(document).ready(main);
